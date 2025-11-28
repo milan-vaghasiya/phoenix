0c6eb27b79103cf432a66e29799d184d112c74d2
milan-vaghasiya <milanvaghasiya@nativebittechnologies.com>
2025-11-28 15:40:02 +0530

    SITE HISTORY REPORT completed with pdf and excel generate

--- application/controllers/reports/SiteHistoryReport.php
+++ application/controllers/reports/SiteHistoryReport.php
@@ -1,7 +1,10 @@
 <?php
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 ini_set('max_execution_time', 500);
 ini_set('memory_limit', '1024M');
-
 class SiteHistoryReport extends MY_Controller
 {
     private $site_history = "report/site_history_report/site_history";  
@@ -38,15 +41,15 @@ class SiteHistoryReport extends MY_Controller
             $projectData = $this->projectHistory->getProjectHistory($data);
             $tbody = ""; $i = 1; 
 			
-			if(!empty($data['file_type']) && $data['file_type'] == 'PDF' && empty($projectData)){
+			if(!empty($data['file_type']) && ($data['file_type'] == 'PDF' || $data['file_type'] == 'excel') && empty($projectData)){
 				redirect(base_url('reports/siteHistoryReport')); 
-			}
+			}		
 
             if(!empty($projectData)){
 				$chunks = array_chunk($projectData, 500);
 
 				foreach($chunks as $chunk){		
-					foreach ($chunk as $row) { 			
+					foreach ($chunk as $row) { 	
 						$media_file = (!empty($row->media_file) ? '<a href="'.$row->media_file.'" target="_blank" download=""><img src="'.$row->media_file.'" class="img-thumbnails" height="50" width="50"></a>' : 'N/A');
 
 						$tbody .= '<tr>
@@ -60,23 +63,22 @@ class SiteHistoryReport extends MY_Controller
 					}
 				}
 				
+				$reportTitle = 'Site History Report';
+				$thead = '<tr style="background:#dddddd;">
+						<th colspan="6">'.$reportTitle.'</th>
+					</tr>							
+					<tr style="background:#dddddd;">
+						<th style="min-width:50px;">#</th>
+						<th style="min-width:200px;">Project Name</th>
+						<th style="min-width:120px;">Created By</th>
+						<th style="min-width:100px;">Created Date</th>
+						<th style="min-width:150px;">Message</th>
+						<th style="min-width:100px;">Media</th>
+					</tr>'; 
+
 				//Generate PDF
 				if(!empty($data['file_type']) && $data['file_type'] == 'PDF')
-				{
-					$reportTitle = 'Site History Report';
-
-					$thead = '<tr style="background:#dddddd;">
-								<th colspan="6">'.$reportTitle.'</th>
-							</tr>							
-							<tr style="background:#dddddd;">
-								<th style="min-width:50px;">#</th>
-								<th style="min-width:200px;">Project Name</th>
-								<th style="min-width:120px;">Created By</th>
-								<th style="min-width:100px;">Created Date</th>
-								<th style="min-width:150px;">Message</th>
-								<th style="min-width:100px;">Media</th>
-							</tr>';  
-										
+				{										
 					$mpdf = new \Mpdf\Mpdf();
 					$pdfFileName = 'SiteHistoryReport_'.str_replace(["/","-"],"_",date('d-m-Y')).'.pdf';          
 					$stylesheet = file_get_contents(base_url('assets/css/pdf_style.css'));
@@ -90,7 +92,70 @@ class SiteHistoryReport extends MY_Controller
 					$mpdf->WriteHTML('</tbody></table>');
 
 					$mpdf->Output($pdfFileName, 'I');
-					ob_clean();	
+					ob_clean();	exit;
+				}
+				else if (!empty($data['file_type']) && $data['file_type'] == 'excel') {
+					$spreadsheet = new Spreadsheet();
+					$sheet = $spreadsheet->getActiveSheet();
+					$sheet->setTitle($reportTitle);
+
+					$sheet->setCellValue("A1", "#");
+					$sheet->setCellValue("B1", "Project Name");
+					$sheet->setCellValue("C1", "Created By");
+					$sheet->setCellValue("D1", "Created Date");
+					$sheet->setCellValue("E1", "Message");
+					$sheet->setCellValue("F1", "Media");
+
+					$sheet->getStyle("A1:F1")->getFont()->setBold(true);
+
+					$sheet->getColumnDimension('A')->setAutoSize(true);   
+					$sheet->getColumnDimension('B')->setAutoSize(true);  
+					$sheet->getColumnDimension('C')->setAutoSize(true); 
+					$sheet->getColumnDimension('D')->setAutoSize(true); 
+					$sheet->getColumnDimension('E')->setAutoSize(true); 
+					$sheet->getColumnDimension('F')->setWidth(20);
+
+					$row = 2;
+					foreach ($projectData as $key => $item) {
+						$sheet->setCellValue("A{$row}", $key + 1);
+						$sheet->setCellValue("B{$row}", $item->project_name);
+						$sheet->setCellValue("C{$row}", $item->user_name);
+						$sheet->setCellValue("D{$row}", formatDate($item->created_at, 'd-m-Y h:i:s A'));
+						$sheet->setCellValue("E{$row}", $item->message);
+
+						if (!empty($item->media_file)) {
+							$path = FCPATH . ltrim(parse_url($item->media_file, PHP_URL_PATH), "/");
+
+							if (file_exists($path)) {
+								$targetHeight = 100;
+								$targetWidth  = 100;
+
+								$drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+								$drawing->setName('Image');
+								$drawing->setDescription('Media Image');
+								$drawing->setPath($path);
+
+								$drawing->setHeight($targetHeight);
+								$drawing->setWidth($targetWidth);
+								$drawing->setCoordinates("F{$row}");
+								$drawing->setOffsetX(5);
+								$drawing->setOffsetY(5);
+								$drawing->setWorksheet($sheet);
+
+								$padding = 10;
+								$sheet->getRowDimension($row)->setRowHeight($targetHeight + $padding);
+							}
+						}
+						$row++;
+					}
+
+					$writer = new Xlsx($spreadsheet);
+					$filename = 'SiteHistoryReport_'.str_replace(["/","-"],"_",date('d-m-Y')).'.xlsx';
+
+					ob_clean();
+					header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+					$writer->save("php://output");
+					exit;
 				}
 			}
 			$this->printJson(['status' => 1, 'tbody' => $tbody]);
--- application/views/report/site_history_report/site_history.php
+++ application/views/report/site_history_report/site_history.php
@@ -8,15 +8,14 @@
                         <div class="col-md-4">
                             <h4 class="card-title pageHeader"><?=$pageHeader?></h4>
                         </div>      
-                        <div class="col-md-3">
+                        <div class="col-md-2">
                             <select name="project_id" id="project_id" class="form-control basic-select2">
-                                <option value="">ALL Project</option>
                                 <?php 
-                                if (!empty($projectList)) :
-                                    foreach ($projectList as $row) :
-                                        echo '<option value="'.$row->id.'">'.$row->project_name.'</option>';
-                                    endforeach;
-                                endif;
+                                    if (!empty($projectList)){
+                                        foreach ($projectList as $row){
+                                            echo '<option value="'.$row->id.'">'.$row->project_name.'</option>';
+                                        }
+                                    }
                                 ?>
                             </select>
                         </div>
@@ -24,14 +23,16 @@
                             <input type="date" name="from_date" id="from_date" class="form-control" max="<?=date('Y-m-d')?>" value="<?=date('Y-m-01')?>" />
                             <div class="error fromDate"></div>
                         </div>     
-                        <div class="col-md-3">  
+                        <div class="col-md-4">  
                             <div class="input-group">
                                 <input type="date" name="to_date" id="to_date" class="form-control" value="<?=date('Y-m-d')?>" />
                                 <div class="input-group-append ml-2">
-                                    <button type="button" class="btn waves-effect waves-light btn-success float-right refreshReportData loadData" title="Load Data">
+                                    <button type="button" class="btn waves-effect waves-light btn-success  refreshReportData loadData" title="Load Data">
                                         <i class="fas fa-sync-alt"></i> Load
                                     </button>
-                                    <button type="button" class="btn btn-danger loadData" data-type="PDF" datatip="PDF" flow="down" target="_blank"><i class="fa fa-file-pdf"></i> PDF</button>
+                                    <button type="button" class="btn btn-primary btnLoadData" data-type="excel" datatip="EXCEL" flow="down" target="_blank"><i class="fa fa-file-excel"></i> Excel</button>
+
+                                    <button type="button" class="btn btn-danger btnLoadData" data-type="PDF" datatip="PDF" flow="down" target="_blank"><i class="fa fa-file-pdf"></i> PDF</button>
                                 </div>
                             </div>
                             <div class="error toDate"></div>
@@ -73,37 +74,65 @@
         $(document).on('click','.loadData',function(e){
             e.stopImmediatePropagation();e.preventDefault();
             $(".error").html("");
-            
-            var valid = 1;
-            var project_id = $('#project_id').val();
-            var from_date = $('#from_date').val();
-            var to_date = $('#to_date').val();
-            var type = $(this).data('type');
+                        
+            loadData();
+        }); 
 
-            if($("#from_date").val() == ""){$(".fromDate").html("From Date is required.");valid=0;}
-            if($("#to_date").val() == ""){$(".toDate").html("To Date is required.");valid=0;}
-            if($("#to_date").val() < $("#from_date").val()){$(".toDate").html("Invalid Date.");valid=0;}
-
-            if(valid)
-            {
-                var sendData = {project_id:project_id, from_date:from_date, to_date:to_date, file_type:type};
-                if(type == 'PDF'){
-                    var url =  base_url + controller + '/getSiteHistory/' + encodeURIComponent(window.btoa(JSON.stringify(sendData)));
-                    window.open(url,'_blank');
-                }else{
-                    $.ajax({
-                        url: base_url + controller + '/getSiteHistory',
-                        data: sendData,
-                        type: "POST",
-                        dataType:'json',
-                        success:function(data){                            
-                            $("#reportTable").dataTable().fnDestroy();
-                            $("#tbodyData").html(data.tbody);
-                            reportTable();
-                        }
-                    });
-                }
-            }
-        });   
+        $(document).on('click','.btnLoadData',function(e){
+            var type = $(this).data('type');                   
+            loadData(type);
+        });
     });
+
+    function loadData(type = ''){
+        var valid = 1;
+        var project_id = $('#project_id').val();
+        var from_date = $('#from_date').val();
+        var to_date = $('#to_date').val();
+
+        if($("#from_date").val() == ""){$(".fromDate").html("From Date is required.");valid=0;}
+        if($("#to_date").val() == ""){$(".toDate").html("To Date is required.");valid=0;}
+        if($("#to_date").val() < $("#from_date").val()){$(".toDate").html("Invalid Date.");valid=0;}
+
+        if(valid)
+        {
+            var sendData = {project_id:project_id, from_date:from_date, to_date:to_date, file_type:type};
+            var ajaxOptions = {
+                url: base_url + controller + '/getSiteHistory',
+                data: sendData,
+                method: "POST"
+            };
+
+            if (type == 'excel' || type == 'PDF') {
+                ajaxOptions.xhrFields = { responseType: 'blob' };
+            } else {
+                ajaxOptions.dataType = 'json';
+            }
+
+            var today = new Date();
+            var date = ("0" + today.getDate()).slice(0, 2) + "-" + ("0" + (today.getMonth() + 1)).slice(0, 2) + "-" + today.getFullYear();
+            var cleanDate = date.replace(/\//g, "_").replace(/-/g, "_");
+            var file_name = "SiteHistoryReport_" + cleanDate;
+
+            $.ajax(ajaxOptions).done(function (data) {
+                if (type == 'excel' || type == 'PDF') {
+                    var today = new Date();
+                    var date = ("0" + today.getDate()).slice(0, 2) + "-" + ("0" + (today.getMonth() + 1)).slice(0, 2) + "-" + today.getFullYear();
+                    var cleanDate = date.replace(/\//g, "_").replace(/-/g, "_");
+                    var file_name = "SiteHistoryReport_" + cleanDate;
+                    
+                    var blob = new Blob([data]);
+                    var link = document.createElement('a');
+                    link.href = window.URL.createObjectURL(blob);
+    
+                    link.download = (type == "excel" ? file_name+".xlsx" : file_name+".pdf");
+                    link.click();
+                } else {
+                    $("#reportTable").dataTable().fnDestroy();
+                    $("#tbodyData").html(data.tbody);
+                    reportTable();
+                }
+            });
+        }
+    }
 </script>
